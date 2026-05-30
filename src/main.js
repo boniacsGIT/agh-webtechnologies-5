bee0211cc367c3be73380240eb643cb706ccc815
boniacsGIT <elektryczneboogaloo@gmail.com>
2026-05-30 23:11:50 +0200

    zad 6

--- src/main.js
+++ src/main.js
@@ -1,36 +1,88 @@
 import './style.css'
 import dayjs from 'dayjs'
 
-const birthdayForm = document.getElementById('birthdayForm')
-const birthdayDialog = document.getElementById('birthdayDialog')
-const birthday = document.getElementById('birthday')
-const birthdayDialogText = document.getElementById('birthdayDialogText')
-const supplementaryDialogText = document.getElementById('supplementaryDialogText')
-const closeDialog = document.getElementById('closeDialog')
+const main = document.getElementById('main')
+const sorter = document.getElementById('sorter')
+const articleForm = document.getElementById('article-form');
 
-birthdayForm.addEventListener('submit', function(event) {
-    event.preventDefault()
+const apiKey = import.meta.env.VITE_APIKEY
 
-    const date = dayjs(birthday.value)
-    const difference = parseInt(date.diff(dayjs(), 'day'))
+function createArticles() {
+    main.textContent = ""
 
-    if (difference < 0) {
-        birthdayDialogText.innerText = 'miales/as urodziny ' + Math.abs(difference) + ' dni temu'
-    } else {
-        birthdayDialogText.innerText = 'masz urodziny za ' + difference + ' dni'
-    }
+    const url = `https://pncfveumuyjjkczxwgyu.supabase.co/rest/v1/article?order=${sorter.value}`
+    
+    fetch(url, {
+    headers: {
+         "apikey": apiKey, 
+         "Authorization": `Bearer ${apiKey}` 
+        },
+    }).then((response) => {
+        return response.json()
+    }).then((data) => {
+        console.log(data)
+        data.forEach((articleData) => {
+            const newArticle = document.createElement('article')
+            newArticle.id = articleData.id
+            newArticle.className = "pt-5 pl-3"
 
-    if (date.isSame(dayjs(), 'day')) {
-        supplementaryDialogText.innerText = 'wszystkiego najlepszego!'
-    } else if (date.isSame(dayjs(), 'week')) {
-        supplementaryDialogText.innerText = 'masz urodziny w tym tygodniu'
-    } else {
-        supplementaryDialogText.innerText = ''
-    }
+            const title = document.createElement('h2')
+            title.className = "text-2xl font-bold"
+            title.innerText = articleData.title
+            newArticle.appendChild(title)
 
-    birthdayDialog.showModal()
+            const subtitle = document.createElement('h3')
+            subtitle.className = "text-xl font-bold"
+            subtitle.innerText = articleData.subtitle
+            newArticle.appendChild(subtitle)
+
+            const author = document.createElement("address")
+            author.className = "text-m"
+            author.innerText = articleData.author
+            newArticle.appendChild(author)
+
+            const date = document.createElement("time")
+            date.className = "text-m"
+            date.innerText = dayjs(articleData.created_at).format('DD-MM-YYYY')
+            newArticle.appendChild(date)
+
+            const paragraph = document.createElement("p")
+            paragraph.className = "text-s pt-1"
+            paragraph.innerText = articleData.content
+            newArticle.appendChild(paragraph)
+
+            main.appendChild(newArticle)
+        })
+    })
+}
+
+articleForm.addEventListener("submit", function(event) {
+  event.preventDefault()
+
+  const newArticle = {
+    title: document.getElementById("title").value,
+    subtitle: document.getElementById("subtitle").value,
+    author: document.getElementById("author").value,
+    created_at: dayjs(document.getElementById("created_at").value || undefined), 
+    content: document.getElementById("content").value
+  };
+
+  const url = "https://pncfveumuyjjkczxwgyu.supabase.co/rest/v1/article"
+
+  fetch(url, {
+    method: "POST",
+    headers: {
+      "apikey": apiKey,
+      "Authorization": `Bearer ${apiKey}`,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(newArticle)
+  }).then((response) => {
+    articleForm.reset()
+    createArticles()
+  })
 })
 
-closeDialog.addEventListener('click', function(event) {
-    birthdayDialog.close()
-})
+sorter.addEventListener("change", createArticles)
+
+createArticles()
